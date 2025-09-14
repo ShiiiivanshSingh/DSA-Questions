0e6f74b11c2da8bac3df1d021e04224884f2371d
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2025-09-14 22:13:20 +0530

    Time: 285 ms (80%), Space: 34.1 MB (60%) - LeetHub

--- 3873-subsequence-sum-after-capping-elements/3873-subsequence-sum-after-capping-elements.cpp
+++ 3873-subsequence-sum-after-capping-elements/3873-subsequence-sum-after-capping-elements.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     vector<bool> subsequenceSumAfterCapping(vector<int>& nums, int k) {
+
+        // hallo !! myau myau stalker ヾ(＠⌒ー⌒＠)ノ
+
+
         sort(nums.begin(),nums.end());
         int n = nums.size();
         vector<bool> dp(k+1, false);
         vector<bool> answer(n, false);
         dp[0] = true;
+
 
         for(int i = 0, x=1; x<=n;x++){
             while(i<n && nums[i] <x){
@@ -14,7 +19,7 @@ public:
                 }
                 i++;
             }
-            int myau=n-i;
+            int myau=n-i; // ~~~///(^v^)\\\~~~
             for(int a=0;a<=myau && k>= a*x; a++){
                 if(dp[k-a*x] == 1){
                     answer[x-1] =1;
