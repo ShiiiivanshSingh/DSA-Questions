f5ccc6c183865a87bfc8040c5ab88a8c5e0844a6
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2025-09-15 22:40:50 +0530

    Time: 0 ms (100%), Space: 101.9 MB (85.35%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int row =  grid.size();
-        int col = grid[0].size();
-        int p =0 ;
-    for(int i=0;i<row;i++){
-        for(int j=0;j<col;j++){
-             if(grid[i][j] == 1){
-                //dishao me udta jaise ubas
-                if((i>0 && grid[i-1][j] == 0) || i ==0) p +=1; //upr
-                if((i<row -1 && grid[i+1][j] == 0) || i ==row -1) p +=1; //neche
-                if((j<col -1 && grid[i][j+1] == 0) || j ==col -1) p +=1; //r
-                if((j>0 && grid[i][j-1] == 0) || j ==0) p +=1; //l
-             }
+    int per = 0;  
+    int cols = grid[0].size();
+    int rows = grid.size();  
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j] == 1){
+                    per +=4;
+                    if(i>0 && grid[i-1][j] == 1) per -=2;
+                    if(j>0 && grid[i][j-1] == 1) per -=2;
+                }   
+            }
         }
-
-        }
-
-
-        return p;
+        return per;
     }
 };
