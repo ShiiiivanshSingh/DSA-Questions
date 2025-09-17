daeef24b89cb3e423ffdcb6642dd39e19484526a
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2025-09-18 00:51:19 +0530

    Time: 51 ms (42.71%), Space: 61.8 MB (34.89%) - LeetHub

--- 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
+    //  leetcode X ricknmorty when????
+    //  meow from Earth C-137
     bool allowed(vector<int>& position, int m, int distance){
         //check if yeh allowed hai?
-        int count  = 1;
+        int summer  = 1;
         int last = position[0];
         for(int i = 1; i < position.size(); i++){
             if(position[i] - last >= distance){
-                count++;
+                summer++;
                 last = position[i];
             }
-            if(count >= m) return 1;
+            if(summer >= m) return 1;
         }
         return 0;
     }
     int maxDistance(vector<int>& position, int m) {
         sort(position.begin(), position.end()); 
         // b r a t  b i n a r y s e a r c h
-        int low = 1, high = position.back() - position.front();
-        int rnm = 0;
-        while(low<=high) {
-        int mid = low + (high - low)/2;
+        int morty = 1, rick = position.back() - position.front();
+        int ricknmorty = 0;
+        while(morty<=rick) {
+        int mid = morty + (rick - morty)/2;
             if(allowed(position, m , mid)){
-                rnm = mid;
-                low = mid + 1;
+                ricknmorty = mid;
+                morty = mid + 1;
             } else {
-                high = mid - 1;
+                rick = mid - 1;
             }
         }
-        return rnm;
+        return ricknmorty;
     }
 };
