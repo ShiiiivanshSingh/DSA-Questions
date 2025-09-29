167c5f444861a67d2a20f5b8ec92550e587cb32b
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2025-09-29 23:01:36 +0530

    Time: 11 ms (57.75%), Space: 21.1 MB (34.56%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
 int count =0;
-    void ee(string s,int left, int right){
+    void wess(string s,int left, int right){
             while(left>=0 && right<s.size() && s[left] == s[right]){
                 count++;
                 left--;
                 right++;
-            }
-            
-    }
+            }}
     int countSubstrings(string s) {
-        vector<int> ss;
+        // vector<int> ss;
        
        for(int i=0;i<s.size();i++){
-       ee(s, i,i);
-       ee(s, i,i+1);
+       wess(s, i,i);
+       wess(s, i,i+1);
        }
         return count;
 
