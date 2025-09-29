1950d05047f1bd89b913ab413ac704c467de598b
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2025-09-29 22:03:02 +0530

    Time: 0 ms (100%), Space: 7.8 MB (72.91%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-    if (n <= 2) return n;
+        
+        int first=1 ,second=2;
+        if(n ==0) return 0;
+        if(n ==1) return 1;
 
-    int first = 1, second = 2;
-
-    for (int i = 3; i <= n; i++) {
-        int current = first + second;
-        first = second;
-        second = current;
-    }
-
+        for(int i=3;i<=n; i++){
+            int current = first + second;
+            first = second;
+            second = current;
+        }
     return second;
-}
-
+    }
 };
