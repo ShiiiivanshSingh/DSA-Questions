23b4a023be1228d0d572037ddf44661e1f1c284a
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2025-10-02 01:28:18 +0530

    Time: 0 ms (100%), Space: 7.7 MB (99.13%) - LeetHub

--- 0910-nth-magical-number/0910-nth-magical-number.cpp
+++ 0910-nth-magical-number/0910-nth-magical-number.cpp
@@ -1,34 +1,40 @@
 // i%a==0 || i%b ==0
 // totaL = (x/a + x/b) - x/lcm(a,b)
+
+
 class Solution {
 public:
-    const long long mod = 1e9+7; // constraint
+    using ll = long long;
+    const ll mod = 1e9+7; 
 
 
-    long long lc_m(long long a,long long b){
+    ll lc_m(ll a,ll b){
             return a/gcd(a,b)*b; // gcd*lcm= num1*num2 
         }
 
     // removed folowing due to bottleneck / tle:(  
     // say a = 29911, b = 37516   it willl give tle hece gcd methid is better
-    // long long lc_m(long long a,long long b){
-    //     long long count = max(a,b); 
+    // ll lc_m(ll a,ll b){
+    //     ll count = max(a,b); 
     //     while(1){ 
     //         if(count%a==0 && count%b==0) return count; 
     //         count++; 
     //         } 
     //     }
 
+
+//actual funtion here
     int nthMagicalNumber(int n, int a, int b) {
         
-        long long lcm = lc_m(a,b); //lcm to avoid 2x counting
-        long long low = min(a,b);
-        long long high = 1LL* n * low; 
+        ll lcm = lc_m(a,b); //lcm to avoid 2x counting
+        ll low = min(a,b);
+        ll high = 1LL* n * low; 
         // for self: 1LL-> to force the calculation in 64bit otherwise garbage vals
 
         while(low<high){ //binary search
-            long long mid = (low+high)/2;
-            long long count = (mid/a + mid/b) - mid/lcm; // totaL = (x/a + x/b) - x/lcm(a,b)
+        
+            ll mid = (low+high)/2;
+            ll count = (mid/a + mid/b) - mid/lcm; // totaL = (x/a + x/b) - x/lcm(a,b)
 
             if(count<n) low = mid+1;
             else high = mid;
