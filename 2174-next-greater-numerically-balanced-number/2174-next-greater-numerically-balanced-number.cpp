50f6b329a9c793cfb2d92a5a7b530e59e345ac43
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2025-10-24 13:53:47 +0530

    Time: 87 ms (45.23%), Space: 8.9 MB (76.38%) - LeetHub

--- 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.cpp
+++ 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    bool fun(int x)
-    {
+    bool achaa(int x){
         int digi[10] = {0};
-        while(x > 0)
-        {
+        while(x > 0){
             digi[x%10]++;
             x /= 10;
         }
-        for(int i=0;i<=9;i++)
-        {
-            if(digi[i] > 0)
-            {
-                if(i != digi[i])
-                {
-                    return false;
+        for(int i=0;i<=9;i++){
+            if(digi[i] > 0){
+                if(i != digi[i]){
+                    return 0;
                 }
             }
         }
-        return true;
+        
+        return 1;
     }
     int nextBeautifulNumber(int n) {
-        int ans = n+1;
-        while(fun(ans) == false)
-        {
-            ans++;
-        }
-        return ans;
+        int myau = n+1;
+
+        while(achaa(myau) == 0) myau++;
+        
+        return myau;
     }
 };
