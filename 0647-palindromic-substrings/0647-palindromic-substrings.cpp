013d3666606481e124b5bc53301c2f4d6c1c5cc5
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2025-10-30 10:34:33 +0530

    Time: 10 ms (57.54%), Space: 21 MB (39.12%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-int count =0;
-    void wess(string s,int left, int right){
-            while(left>=0 && right<s.size() && s[left] == s[right]){
-                count++;
-                left--;
-                right++;
-            }}
-    int countSubstrings(string s) {
-        // vector<int> ss;
-       
-       for(int i=0;i<s.size();i++){
-       wess(s, i,i);
-       wess(s, i,i+1);
-       }
-        return count;
-
+    int ans =0;
+    void myau(string s , int  left, int right){
+        while( left >=0 && right< s.size() && (s[left] == s[right])){
+            ans++;  left--;   right++;
+        }
     }
+    int countSubstrings(string s) {
+        for(int i=0;i<s.size();i++){
+            myau(s, i , i);
+            myau(s, i  , i+1);
+        }
+    return ans;
+    }
+
 };
+
