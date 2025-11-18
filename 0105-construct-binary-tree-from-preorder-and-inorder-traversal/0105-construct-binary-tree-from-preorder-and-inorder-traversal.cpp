cd96f15dd654ea4c3c5fd053b6efc5a22d99c31b
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2025-11-18 12:06:41 +0530

    Time: 11 ms (21.88%), Space: 27 MB (89.51%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,34 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-    int preIndex = 0;
-    
-    TreeNode* helper(vector<int>& preorder, vector<int>& inorder, int inStart, int inEnd) {
-     
-        if (inStart > inEnd) 
-            return nullptr;
-        
-        
-        TreeNode* node = new TreeNode(preorder[preIndex++]);
-        
-       
-        if (inStart == inEnd)
-            return node;
-        
-        
-        int inIndex = inStart;
-        while (inIndex <= inEnd && inorder[inIndex] != node->val) {
-            inIndex++;
-        }
-        
-      
-        node->left = helper(preorder, inorder, inStart, inIndex - 1);
-        node->right = helper(preorder, inorder, inIndex + 1, inEnd);
-        
+public:
+int current = 0;
+    TreeNode* solve(vector<int>& inorder , vector<int>& preorder , int start , int end ){
+        if(start > end) return NULL;
+
+    TreeNode* node = new TreeNode(preorder[current++]);
+    if(start == end){
+
         return node;
     }
+    int  i = start;
+    while(i <= end && inorder[i]!= node->val){
+        i++;
+    }
+    node->left = solve(inorder , preorder , start ,i -1);
+    node->right = solve(inorder , preorder , i+1 ,end);
     
-public:
+        return node;
+    } 
+    
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        preIndex = 0;
-        return helper(preorder, inorder, 0, inorder.size() - 1);
+        int size = inorder.size();
+
+        return solve(inorder , preorder,  0 , size-1);
     }
 };
