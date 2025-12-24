03c75d1cc2e0f66f7caa3d5136606854a9061882
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2025-12-24 11:26:49 +0530

    Time: 12 ms (75.26%), Space: 58.2 MB (53.57%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,69 +10,40 @@
  */
 class Solution {
 public:
-// 3 in 1
-// find middle eleement
-// mereg 2 sorted lsit
-// merge sort
-//rec sorting 
-
-// from -> leetcode.com/problems/middle-of-the-linked-list
-// lc 876
-    ListNode* mid(ListNode* head) {
+    ListNode* mid(ListNode* head){
         ListNode* slow = head;
-        ListNode* fast = head->next;  //for merge sort
-    
+        ListNode* fast = head->next;
+
         while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
         }
-        return slow;
+    return slow;
     }
-// from -> leetcode.com/problems/merge-two-sorted-lists
-// merge 2 sorted lists lc 21
-    ListNode* merge(ListNode* list1, ListNode* list2) {
-        ListNode dummy(0);
-        ListNode* tail = &dummy;
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if(!l1 ) return l2;
+        if(!l2) return l1;
+        ListNode* result;
 
-        while (list1 && list2) {
-            if (list1->val <= list2->val) {
-                tail->next = list1;
-                list1 = list1->next;
-
-                
-            } else {
-                tail->next = list2;
-                list2 = list2->next;
-            }
-            tail = tail->next;
+        if(l1->val > l2->val){
+            result = l2;
+            result->next = merge(l1, l2->next);
+        }   else {
+            result = l1;
+            result->next = merge(l1->next ,l2);
         }
-
-        if (list1) {
-            tail->next = list1;
-
-
-        } else {
-            tail->next = list2;
-        }
-
-
-
-
-        return dummy.next;
+    return result;
     }
 
-// merge sort
     ListNode* sortList(ListNode* head) {
-         if(!head || !head->next) return head;       
-    
-        ListNode* middle = mid(head);
+        if(!head || !head->next) return head;
+
+        ListNode* middle= mid(head);
         ListNode* right = middle->next;
         middle->next = NULL;
+        ListNode* x = sortList(right);
+        ListNode* y =sortList(head);
 
-        ListNode* RIGHT = sortList(right);
-        ListNode* LEFT = sortList(head);
-
-        return merge(RIGHT, LEFT);
-    
+        return merge(x ,y);
     }
 };
