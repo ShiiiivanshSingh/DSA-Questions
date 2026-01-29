f759dc148116434f6af01ecc33a3054351527294
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-01-29 20:04:19 +0530

    Time: 80 ms (44.02%), Space: 95.9 MB (39.09%) - LeetHub

---
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+// weighted interval scheduling
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+
+        int n= startTime.size();
+        vector<vector<int>> jobs;
+        for(int i=0 ;i<n;i++){
+            jobs.push_back({endTime[i], startTime[i] ,profit[i]});
+        }
+        vector<int> dp(n);
+        sort(jobs.begin(), jobs.end());
+        dp[0] = jobs[0][2]; //profit entires
+        for(int i=1 ;i<n;i++){
+            int take = jobs[i][2];
+            int left =0, right= i-1;
+            int index = -1;
+            while(left <= right){
+                int mid= (left + right) /2;
+                if(jobs[mid][0] <= jobs[i][1]){
+                    index = mid;
+                    left = mid + 1;
+                } else right = mid -1;
+            }
+            if(index != -1){    
+                take += dp[index];
+            }
+
+            dp[i] = max(take, dp[i-1]);
+
+        }
+    return dp[n-1];
+    }
+};
