bf0abb1ef97e767cbb7000250f40e0b2e01d24ac
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-01-30 10:17:07 +0530

    Time: 50 ms (7.6%), Space: 59.7 MB (86.2%) - LeetHub

--- 2263-maximum-running-time-of-n-computers/2263-maximum-running-time-of-n-computers.cpp
+++ 2263-maximum-running-time-of-n-computers/2263-maximum-running-time-of-n-computers.cpp
@@ -1,31 +1,30 @@
 using ll = long long;
 class Solution {
 public:
-    bool canHold(vector<int>& batteries, ll mid , int n){
-        ll need  = n* mid;
-        for (int b : batteries) {
-            need -= min((long long)b, mid);
-                if (need <= 0) return true;  
+    bool helper( vector<int>& batteries, int n, ll mid){
+        long long need = n * mid;
+        for (long long i =0;i<batteries.size();i++){
+        
+            need -=  min((ll)batteries[i] , mid);
+            if(need<=0) return 1;
         }
         return 0;
     }
-    
     long long maxRunTime(int n, vector<int>& batteries) {
         ll ans = 0;
-        ll max_time = 0 ; 
+        ll max_time = 0;
         sort(batteries.begin() , batteries.end());
-        for(int  i =0; i< batteries.size() ; i++) max_time += batteries[i];
-
+        for(int i=0; i<batteries.size();i++){
+            max_time += batteries[i];
+        }
         ll low = 0, high = max_time/n;
-
-        while(high >= low){
-            ll mid =  low + (high - low)/2;
-
-            if(canHold(batteries, mid ,n)){
+        while(high >=low){
+            ll mid = low + (high - low)/2;
+            if(helper(batteries, n ,mid)){
                 ans = mid;
                 low = mid+1;
-            } else high = mid - 1; 
-        }
-  return ans;
+            } else high = mid -1;
+        } 
+        return ans;
     }
 };
