0c86ef22b0e4083d7acf301f4fe0b80edc1446aa
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-02-02 23:28:48 +0530

    Time: 2 ms (88.1%), Space: 12.3 MB (69.05%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -6,13 +6,13 @@ public:
 
         if(abs(target)> total) return 0;
         if ((target + total) % 2 != 0) return 0;  
+        
         int x = (total +target) /2;
+
         vector<int> dp(x+1, 0);
         dp[0] =1;
         for(int i: nums){
-            for(int j=x; j>=i;j--){
-                dp[j] = dp[j] + dp[j -i];
-            }
+            for(int j=x; j>=i;j--)  dp[j] = dp[j] + dp[j -i];
         }
         return dp[x];
     }
