460db22e8e7d8957448854b59a1ad3cf75658f1d
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-02-02 23:28:21 +0530

    Time: 0 ms (100%), Space: 12.3 MB (69.05%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int total =0;
+        for(int i: nums) total+=i;
+
+        if(abs(target)> total) return 0;
+        if ((target + total) % 2 != 0) return 0;  
+        int x = (total +target) /2;
+        vector<int> dp(x+1, 0);
+        dp[0] =1;
+        for(int i: nums){
+            for(int j=x; j>=i;j--){
+                dp[j] = dp[j] + dp[j -i];
+            }
+        }
+        return dp[x];
+    }
+};
