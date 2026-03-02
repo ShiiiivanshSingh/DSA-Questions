bc91667a29d23c06da1029d91ce2a07b0a72b108
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-03-02 22:37:05 +0530

    Time: 0 ms (100%), Space: 10 MB (20.34%) - LeetHub

--- 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.cpp
+++ 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     bool isFascinating(int n) {
-        //ez sol-> return n == 192 || n == 219 || n == 273 || n == 327; 
+        //ez sol1-> 
+        // return n == 192 || n == 219 || n == 273 || n == 327; 
         set<char> st;      
         string s1 =to_string(n);
         string s2 =to_string(n * 2);
         string s3 =to_string(n * 3);
+
         string final = s1 + s2 + s3;
-        for(auto &i : final){
-            if(st.count(i) || i == '0') return 0;
-            st.insert(i);
-        }
-        if(st.size() == 9) return 1;
+        // sol2->
+        // for(auto &i : final){
+        //     if(st.count(i) || i == '0') return 0;
+        //     st.insert(i);
+        // }
+        // if(st.size() == 9) return 1;
+        // else return 0;
+
+        // sol3->
+        sort(final.begin(),final.end());
+        if(final =="123456789" )return 1;
         else return 0;
     }
 };
