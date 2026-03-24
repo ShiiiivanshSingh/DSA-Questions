d78f42f8f9cbaaa34bc7b925ef0a3065b84d2824
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-03-24 12:11:02 +0530

    3031-CONSTRUCT-PRODUCT-MATRIX Stats: Time: 25 ms (89.67%), Space: 131.9 MB (97.42%) - LeetHub

--- 3031-construct-product-matrix/3031-construct-product-matrix.cpp
+++ 3031-construct-product-matrix/3031-construct-product-matrix.cpp
@@ -1,41 +1,27 @@
 class Solution {
 public:
-using ll = long long;
-    vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+    static vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
+        const int MOD=12345;
+        int n=grid.size(), m=grid[0].size();
         vector<vector<int>> ans(n, vector<int>(m));
 
-        vector<int> arr(n * m);
-        for(int i=0;i<n;i++)
-           for(int j=0;j<m;j++)
-             arr[i*m + j]= grid[i][j];
+        uint64_t prefix =1, suffix =1;
 
-        vector<int> res = productExceptSelf(arr);
-
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++)
-                ans[i][j] = res[i * m + j] % 12345;
-
+        for(int i=0;i<n;i++){ // prefix 
+            for(int j=0;j<m;j++){
+                ans[i][j]= prefix;
+                prefix *= grid[i][j];
+                prefix  %= MOD;
+            }
+        }
+        for(int i=n-1; i>=0; i--){ // building suffix
+            for(int j=m-1; j>=0; j--){
+                ans[i][j] *= suffix;
+                ans[i][j] %= MOD;
+                suffix  *= grid[i][j];
+                suffix  %= MOD;
+            }
+        }
         return ans;
     }
-
-
-    vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        int prefix =1, suffix =1;
-        vector<int> ans(n,0), pref(n,1), suff(n,1);
- 
-        for(int i=1;i<n;i++)
-            pref[i] = (1LL * pref[i-1] * nums[i-1]) % 12345;
-   
-        for(int i=n-2;i>=0;i--)
-            suff[i] = (1LL * suff[i+1] * nums[i+1]) % 12345;
-     
-        for(int i=0;i<n;i++)
-            ans[i] = (1LL * suff[i] * pref[i]) % 12345;
-        
-        return ans;
-    }
-
 };
