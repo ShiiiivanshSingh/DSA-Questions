ef13e8bf9458413330c2c4b569dc6d1a9964225b
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-03-24 10:27:22 +0530

    1833-FIND-THE-HIGHEST-ALTITUDE Stats: Time: 0 ms (100%), Space: 10.9 MB (44%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
@@ -4,14 +4,10 @@ public:
         int ans=0;
         int curr = 0;
         for(int i=0;i<gain.size();i++){
-
-
             ans = ans + gain[i];
 
-            // int now = gain[i];
-            // int curr = gain[i] -gain[i-1];
             curr = max(ans, curr);
-            cout  << " "<< curr<< " "<< ans << "\n";
+            // cout  << " "<< curr<< " "<< ans << endl;
         }
         return curr;
     }
