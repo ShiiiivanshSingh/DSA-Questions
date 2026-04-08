fc8c935392e4de832eb7ddf30242a4ce69c17071
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-04-08 18:45:52 +0530

    0365-WATER-AND-JUG-PROBLEM Stats: Time: 0 ms (100%), Space: 7.6 MB (99.63%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
 
     bool canMeasureWater(int x, int y, int target) {
-        if (target > x + y) return false;
-        if (target == 0) return true;
-        return target % __gcd(x, y) == 0;
+        if (target > x + y) return 0;
+        if (target == 0) return 1;
+        if( target % __gcd(x, y) == 0) return 1;
+        return 0;
     }
 };
