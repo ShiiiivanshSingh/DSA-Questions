16f4630936cfa96aa30869381f3ea3b6d41f961d
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-04-12 21:53:24 +0530

    4280-COUNT-DIGIT-APPEARANCES Stats: Time: 0 ms (100%), Space: 36.9 MB (88.89%) - LeetHub

--- 4280-count-digit-appearances/4280-count-digit-appearances.cpp
+++ 4280-count-digit-appearances/4280-count-digit-appearances.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     int countDigitOccurrences(vector<int>& nums, int digit) {
-       map<int, int> mp;
-       int ans =0;
-       for(auto i : nums){
-        if(i==0) mp[0]++;
-        while(i >0){
-            mp[i % 10]++;
-            i /=10;
+        int ans =0;
+        for(int x: nums){
+            if(x == 0 && digit == 0)
+                ans++;
+             
+            while(x>0){
+                if(x%10 == digit) ans++;
+                x /=10;
+            }
         }
-       }
-       return mp[digit]; 
+        return ans;
     }
 };
+        
+
+    //    map<int, int> mp;
+    //    int ans =0;
+    //    for(auto i : nums){
+    //     if(i==0) mp[0]++;
+    //     while(i >0){
+    //         mp[i % 10]++;
+    //         i /=10;
+    //     }
+    //    }
+    //    return mp[digit]; 
