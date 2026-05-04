a63a2d11c0896b3e0015034241246322fb927191
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-05-04 15:51:00 +0530

    4294-MAXIMIZE-FIXED-POINTS-AFTER-DELETIONS Stats: Time: 75 ms (100%), Space: 181.5 MB (84.62%) - LeetHub

---
+++ 4294-maximize-fixed-points-after-deletions/4294-maximize-fixed-points-after-deletions.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+// //     int maxFixedPoints(vector<int>& a) {      
+// //     vector<int> s;
+// 
+// //     for (int i = 0; i < a.size(); i++) {
+// //         if (i >= a[i]) {
+// //             int x = a[i];
+// //             auto it = lower_bound(s.begin(), s.end(), x);
+// //             if (it == s.end()) s.push_back(x);
+// //             else *it = x;
+// //         }
+// //     }
+
+// //     return s.size();
+// //  }
+// // };
+// class Solution {
+// public:
+//     int maxFixedPoints(vector<int>& a) {
+//         vector<int> s;
+
+//         for (int i = 0; i < a.size(); i++) {
+//             if (i >= a[i]) {
+//                 int x = i - a[i];
+//                 auto it = upper_bound(s.begin(), s.end(), x);
+//                 if (it == s.end()) s.push_back(x);
+//                 else *it = x;
+//             }
+//         }
+
+//         return s.size();
+//     }
+int maxFixedPoints(vector<int>& A) {
+    vector<int> s;
+    vector<pair<int,int>> B;
+
+    for (int i = 0; i < A.size(); ++i)
+        if (i >= A[i])
+            B.push_back({i - A[i], A[i]});
+
+    sort(B.begin(), B.end());
+
+    for (auto& p : B) {
+        int x = p.second;
+        auto it = lower_bound(s.begin(), s.end(), x);
+        if (it == s.end()) s.push_back(x);
+        else *it = x;
+    }
+
+    return s.size();
+}
+};
