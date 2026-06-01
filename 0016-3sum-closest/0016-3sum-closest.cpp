c77ece5d4edea3ba803b7e7cbd684e949007855a
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-06-01 17:51:56 +0530

    Improve while loop condition in 3sum-closest
    Refactor while loop condition for clarity.

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -6,7 +6,7 @@ public:
         int meow = nums[0] + nums[1] + nums[2] ;
         for(int i=0;i<n;i++){
             int left = i+1,  right = n-1;
-            while(left< right){
+            while(left< right){ 
                 int sum = nums[left] + nums[right] + nums[i];
                 if(abs(sum - target) < abs(meow -target)) meow = sum;
                 if(sum<target) left++;
