b1ea1618756daf64582c3aa39aa79c3942451135
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-07-18 12:50:06 +0530

    2106-FIND-GREATEST-COMMON-DIVISOR-OF-ARRAY Stats: Time: 0 ms (100%), Space: 16.1 MB (92.83%) - LeetHub

--- 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int findGCD(vector<int>& nums) {
-        int myau = *max_element(nums.begin(), nums.end());
-        int bhau = *min_element(nums.begin(), nums.end());
+        int myau = *max_element(nums.begin(), nums.end()), bhau = *min_element(nums.begin(), nums.end());
         return gcd(myau, bhau);
     }
 };
