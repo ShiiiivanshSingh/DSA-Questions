f14c1f2cc63c523d8ef99db1e7f7cdc3b7be9349
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-08-10 19:59:13 +0530

    1685-STONE-GAME-V Stats: Time: 1010 ms (10.18%), Space: 27.8 MB (32.69%) - LeetHub

--- 1685-stone-game-v/1685-stone-game-v.cpp
+++ 1685-stone-game-v/1685-stone-game-v.cpp
@@ -5,34 +5,31 @@ public:
         int left = 0 , right = n -1, alice =0;
         vector<vector<int>> dp(n, vector<int>(n, 0));
         vector<int> prefix(n + 1, 0);
+
         for (int i = 0; i <n; i++)  
             prefix[i+1] = prefix[i] + stoneValue[i];
-        for (int i = 0; i <n; i++)  
+
+        for (int i = 0; i < n; i++)  
             dp[i][i] = 0;
 
 
         for (int len = 2; len <= n; len++) {
             for (int i = 0; i + len - 1 < n; i++) {
-                    int j = i + len - 1;
+
+                int j = i + len - 1;
+                        
+                for (int mid = i; mid < j; mid++) {
+                    int leftSum = prefix[mid + 1] - prefix[i];
+                    int rightSum = prefix[j + 1] - prefix[mid + 1];
+
+                // if(i == j) dp[i][j] = 0;
+                    if(leftSum > rightSum) dp[i][j] = max(dp[i][j], rightSum + dp[mid + 1][j]);
+                    else if(leftSum < rightSum) dp[i][j] = max(dp[i][j] , leftSum + dp[i][mid]);
+                    else dp[i][j] =  max({dp[i][j], leftSum + dp[i][mid], rightSum + dp[mid + 1][j] });
                     
-            for (int mid = i; mid < j; mid++) {
-                int leftSum = prefix[mid + 1] - prefix[i];
-                int rightSum = prefix[j + 1] - prefix[mid + 1];
-
-               // if(i == j) dp[i][j] = 0;
-
-                if(leftSum > rightSum) {
-                    dp[i][j] = max(dp[i][j], rightSum + dp[mid + 1][j]);
-                }
-                else if(leftSum < rightSum) {
-                    dp[i][j] = max(dp[i][j] , leftSum + dp[i][mid]);
-                }
-                else {
-                    dp[i][j] =  max({dp[i][j], leftSum + dp[i][mid], rightSum + dp[mid + 1][j] });
-                }
+                    }
                 }
             }
-        }
         return dp[0][n-1];
     }
 };
