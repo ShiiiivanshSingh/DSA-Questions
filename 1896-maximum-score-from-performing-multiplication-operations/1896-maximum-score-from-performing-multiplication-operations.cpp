eab1f276bba50dad672830e00094e47cfe96f77f
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-08-11 12:26:53 +0530

    1896-MAXIMUM-SCORE-FROM-PERFORMING-MULTIPLICATION-OPERATIONS Stats: Time: 32 ms (36.77%), Space: 88 MB (39.96%) - LeetHub

--- 1896-maximum-score-from-performing-multiplication-operations/1896-maximum-score-from-performing-multiplication-operations.cpp
+++ 1896-maximum-score-from-performing-multiplication-operations/1896-maximum-score-from-performing-multiplication-operations.cpp
@@ -3,21 +3,27 @@ public:
     vector<vector<int>> dp;
 
     int solve(int left, int op, vector<int>& nums, vector<int>& multipliers) {
-    int n = nums.size();
-    int m = multipliers.size();
-    if (op == m) return 0;
+        int n = nums.size();
+        int m = multipliers.size();
 
-    if (dp[left][op] != -1) return dp[left][op];
-    int right = n - 1 - (op - left);
-    int takeLeft = nums[left] * multipliers[op] + solve(left + 1, op + 1, nums, multipliers);
-    int takeRight = nums[right] * multipliers[op] + solve(left, op + 1, nums, multipliers);
-    return dp[left][op] = max(takeLeft, takeRight);
-} int maximumScore(vector<int>& nums, vector<int>& multipliers) {
-    int n = nums.size();
-    int m = multipliers.size();
-  //  if (op == m)
-    //    return 0;
-    dp.assign(m + 1, vector<int>(m + 1, -1));
+        if (op == m)   return 0;
+        if (dp[left][op] != -1)   return dp[left][op];
 
-    return solve(0, 0, nums, multipliers);
-}};
+        int right = n - 1 - (op - left);
+
+        int pickLeft = nums[left] * multipliers[op] +  solve(left + 1, op + 1, nums, multipliers);
+        int pickRight = nums[right] * multipliers[op] + solve(left, op + 1, nums, multipliers);
+
+        return dp[left][op] = max(pickLeft, pickRight);
+
+    }
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        int n = nums.size();
+        int m = multipliers.size();
+        //  if (op == m)
+        //    return 0;
+        dp.assign(m + 1, vector<int>(m + 1, -1));
+
+        return solve(0, 0, nums, multipliers);
+    }
+};
