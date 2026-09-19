bb6da844c79729a0a192389c2dbfc25fe339f308
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-09-19 16:28:57 +0530

    1501-CIRCLE-AND-RECTANGLE-OVERLAPPING Stats: Time: 87 ms (6.15%), Space: 7.9 MB (35%) - LeetHub

--- 1501-circle-and-rectangle-overlapping/1501-circle-and-rectangle-overlapping.cpp
+++ 1501-circle-and-rectangle-overlapping/1501-circle-and-rectangle-overlapping.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     bool checkOverlap(int r,int xc,int yc,int x1,int y1,int x2,int y2) {
 
-        for(int i=x1;i<=x2;i++){
-            for(int j=y1;j<=y2;j++){
-                int dx=i-xc;
-                int dy=j-yc;
-                if(dx *  dx +dy*  dy<=  r*r ) return 1;
+        for(int i= x1;i<= x2;i++){
+            for(int j= y1;j<= y2;j++){
+                int dx= i- xc, dy=j- yc;
+                if(dx *  dx +dy*  dy <= r*r ) return 1;
             }
         }
-
         return 0;
     }
 };
