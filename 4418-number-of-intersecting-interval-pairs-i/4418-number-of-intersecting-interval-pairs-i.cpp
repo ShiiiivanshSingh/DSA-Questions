a7193d51b6529e304e79ae2836ea90e1c6a4dc80
Shivansh Pratap Singh <137640417+ShiiiivanshSingh@users.noreply.github.com>
2026-09-20 18:16:43 +0530

    4418-NUMBER-OF-INTERSECTING-INTERVAL-PAIRS-I Stats: Time: 4 ms (50.21%), Space: 36.7 MB (11.74%) - LeetHub

---
+++ 4418-number-of-intersecting-interval-pairs-i/4418-number-of-intersecting-interval-pairs-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countIntersectingIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        priority_queue<int, vector<int>, greater<>> pq;
+        sort(intervals.begin(), intervals.end());
+        pq.push(intervals[0][1]);
+        int ans=0;
+        for(int i=1;i<n;i++){
+            while(!pq.empty() && pq.top() < intervals[i][0]){
+                pq.pop();
+            }
+            ans = ans + pq.size();
+            pq.push(intervals[i][1]);
+        }
+        return ans;
+    }
+};
